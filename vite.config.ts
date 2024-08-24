f83ae77db90b8fb443b7f964ae7985e565221bee
Raymond Jacobson <ray@audius.co>
2024-08-23 17:37:57 -0700

    Improve polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import pages from '@hono/vite-cloudflare-pages'
-// import devServer from '@hono/vite-dev-server'
+import devServer from '@hono/vite-dev-server'
 import { UserConfig, defineConfig } from 'vite'
-// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-// import wasm from 'vite-plugin-wasm'
-// import commonjs from '@rollup/plugin-commonjs'
-// import react from '@vitejs/plugin-react'
-// import { defineConfig } from 'vite'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from "rollup-plugin-node-polyfills";
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
@@ -14,9 +10,6 @@ export default defineConfig(({ mode }) => {
     return {
       plugins: [
         react(),
-        pages({
-          entry: 'src/index.tsx'
-        }),
       ],
       optimizeDeps: {
         esbuildOptions: {
@@ -25,7 +18,7 @@ export default defineConfig(({ mode }) => {
           },
           plugins: [
             NodeGlobalsPolyfillPlugin({
-              buffer: true
+              buffer: true,
             })
           ]
         }
@@ -39,29 +32,17 @@ export default defineConfig(({ mode }) => {
         },
       }
     } as UserConfig
-    // config = {
-    //   ...config,
-    //   plugins: [
-    //     react(),
-    //     wasm()
-    //   ],
-    //   build: {
-    //     ...config.build,
-    //     rollupOptions: {
-    //       input: './src/client/main.tsx',
-    //       output: {
-    //         entryFileNames: 'static/client/main.js',
-    //       },
-    //     },
-    //   },
-    // }
   }
+
   return {
     plugins: [
       react(),
       pages({
         entry: 'src/index.tsx'
       }),
+      devServer({
+        entry: 'src/index.tsx'
+      })
     ],
     optimizeDeps: {
       esbuildOptions: {
@@ -74,76 +55,16 @@ export default defineConfig(({ mode }) => {
           })
         ]
       }
+    },
+    build: {
+      rollupOptions: {
+        plugins: [
+          nodePolyfills({
+            crypto: true,
+            fs: true
+          })
+        ]
+      }
     }
   } as UserConfig
-//   let config: UserConfig = {
-//     plugins: [
-//       pages({
-//         entry: 'src/index.tsx'
-//       }),
-//       devServer({
-//         entry: 'src/index.tsx',
-//       }),
-//       wasm(),
-//       commonjs({
-//         include: [/node_modules/],
-//         transformMixedEsModules: true
-//       })
-//     ],
-//     optimizeDeps: {
-//       esbuildOptions: {
-//         define: {
-//           global: 'globalThis'
-//         },
-//         plugins: [
-//           NodeGlobalsPolyfillPlugin({
-//             buffer: true
-//           })
-//         ]
-//       }
-//     }
-//   }
-
-//   if (mode === 'client') {
-//     config = {
-//       ...config,
-//       plugins: [
-//         react(),
-//         wasm()
-//       ],
-//       build: {
-//         ...config.build,
-//         rollupOptions: {
-//           input: './src/client/main.tsx',
-//           output: {
-//             entryFileNames: 'static/client/main.js',
-//           },
-//         },
-//       },
-//     }
-//   }
-//   return config
 })
-
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     pages({
-//       entry: 'src/index.tsx'
-//     }),
-//   ],
-//   optimizeDeps: {
-//     esbuildOptions: {
-//       define: {
-//         global: 'globalThis'
-//       },
-//       plugins: [
-//         NodeGlobalsPolyfillPlugin({
-//           buffer: true
-//         })
-//       ]
-//     }
-//   }
-// })
